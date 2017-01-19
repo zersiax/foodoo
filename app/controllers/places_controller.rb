62ee2e066d8e49c443d9b14b02196acbccc1fb9d
Zersiax <florianbeijers@gmail.com>
2017-01-19 18:59:03 +0000

    made the top nav more useful

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,16 @@ def index
 end
 
 def new
-@ place = Place.new
+@place = Place.new
 end
 
+def create
+  Place.create(place_params) 
+  redirect_to root_path
+  end
+end
+
+private
+def place_params
+params.require(:place).permit(:name, :address, :description)
 end
