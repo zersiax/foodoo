2f692359ff74a0c0ec5662e74e97954aef5a7648
Zersiax <florianbeijers@gmail.com>
2017-02-07 01:23:48 +0000

    typo

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,7 +6,7 @@ class NotificationMailer < ApplicationMailer
   @place_owner = @place.user
   mail(to: @place_owner.email,
                 subject: "A comment has been added to #{@place.name}")
-ende
+end
         
   end
 end
