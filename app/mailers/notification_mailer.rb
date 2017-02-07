851f82b34537f238cb0926bce3b31fe91d6d03fe
Zersiax <florianbeijers@gmail.com>
2017-02-07 01:19:36 +0000

    oops ...mailing works better when you actually fill in code for the mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,5 +2,11 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added (comment)
+        @place = comment.place
+  @place_owner = @place.user
+  mail(to: @place_owner.email,
+                subject: "A comment has been added to #{@place.name}")
+ende
+        
   end
 end
