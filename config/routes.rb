6baeb203a3c899a109bbdfa6191a34492cfde33d
Zersiax <florianbeijers@gmail.com>
2017-02-07 00:38:35 +0000

    fixed routes file?

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :places do 
     resources :comments, only: :create
     resources :photos, only: :create
+resources :users, only: :show
     
   end
   
